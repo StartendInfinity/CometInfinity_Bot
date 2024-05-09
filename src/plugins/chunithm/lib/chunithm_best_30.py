c7606138a654cf738af77abaa66816f1c8fe0c1e
Ekzykes <atlaseuan@163.com>
2024-05-09 13:37:24 +0800

    同步

--- src/plugins/chunithm/lib/chunithm_best_30.py
+++ src/plugins/chunithm/lib/chunithm_best_30.py
@@ -13,6 +13,17 @@ class DrawBest(object):
         self.imgDraw = ImageDraw.Draw(self.img)
         self.draw()
 
+    def _Q2B(self, uchar):
+        """单个字符 全角转半角"""
+        inside_code = ord(uchar)
+        if inside_code == 0x3000:
+            inside_code = 0x0020
+        else:
+            inside_code -= 0xfee0
+        if inside_code < 0x0020 or inside_code > 0x7e: #转完之后不是半角字符返回原来的字符
+            return uchar
+        return chr(inside_code)
+
     def _stringQ2B(self, ustring):
         """把字符串全角转半角"""
         return "".join([self._Q2B(uchar) for uchar in ustring])
